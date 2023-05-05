90a1fa0c3bba9c9cde5d60d5d992062aa94a6632
hoangducchung <hoangducchungdtnt@gmail.com>
2023-05-05 15:34:13 +0700

    adjust color: green -> blue

--- WindowsFormsApp1/Form1.Designer.cs
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -61,7 +61,7 @@
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft YaHei", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.Color.Green;
+            this.label3.ForeColor = System.Drawing.Color.RoyalBlue;
             this.label3.Location = new System.Drawing.Point(328, 47);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(136, 19);
